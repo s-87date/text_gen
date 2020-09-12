57cf8abad55183b8062015abdcdc6eabffdf1374
Shuhei Hanadate <shu87.scarycat@gmail.com>
2020-09-13 01:08:00 +0900

    update train

--- wakachi_processor.sh
+++ wakachi_processor.sh
@@ -1,1 +1,1 @@
-python3 ./src/preprocess.py --input_file ./dat/tachikoma_train.txt --output_file ./dat/tachikoma_train_wakachi.txt --model ./model/jumanpp-2.0.0-rc2/model/jumandic.jppmdl
+python3 ./src/preprocess.py --input_file ./dat/tachikoma_train.txt --output_file ./dat/tachikoma_train_wakachi.txt --model ./model/jumanpp-2.0.0-rc3/model/jumandic.jppmdl
